b2dc560a53c6e184ff0055048af6e493b786a819
karin <doykarin@gmail.com>
2022-01-07 23:43:24 -0300

    initial commit

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+const port = 3000;
+app.use(routes)
+
+app.get('/', (req, res) => res.send('Welcome to e-ponto'));
+
+app.listen(port, () => {
+    console.log(`Application is running on port ${port}.`);
+});
