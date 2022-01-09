92e477f49e35da6ec960546046af70a68889fbc2
karin <doykarin@gmail.com>
2022-01-09 01:02:44 -0300

    add entrytime

--- backend/src/controllers/RegisterController.ts
+++ backend/src/controllers/RegisterController.ts
@@ -1,25 +1,32 @@
 import { Request, Response } from "express";
 import { registerService } from "../services/RegisterService";
+import { isBoolean, isInteger } from "../utils";
 
 export default {
     async insertRegisterDateTime(req: Request, res: Response) {
         let dateTime = req?.body?.dateTime
+        let entryTime = req?.body?.entryTime
 
-        if (!dateTime) {
-            console.log("aaaaa")
+        if (!isInteger(dateTime)) {
             return res.status(400).json({
                 status: 400,
                 message: "dateTime must be present"
             })
         }
 
-        dateTime = await registerService.insertRegisterDateTime(dateTime);
+        if (!isBoolean(entryTime)) {
+            return res.status(400).json({
+                status: 400,
+                message: "entryTime must be present and should be a boolean type"
+            })
+        }
+
+        let registerResponse: any = await registerService.insertRegisterDateTime(dateTime, entryTime);
 
         return res.status(201).json({
             status: 201,
             message: "Created",
-            dateTime: dateTime
+            response: registerResponse
         })
-        // return 
     }
 }
--- backend/src/services/RegisterService.ts
+++ backend/src/services/RegisterService.ts
@@ -2,9 +2,12 @@
 
 class RegisterService {
 
-    async insertRegisterDateTime(dateTime: number): Promise<number> {
+    async insertRegisterDateTime(dateTime: number, entryTime: boolean): Promise<object> {
 
-        return dateTime;
+        return {
+            "dateTime": dateTime,
+            "entryTime": entryTime
+        };
     }
 }
 export let registerService = new RegisterService();
